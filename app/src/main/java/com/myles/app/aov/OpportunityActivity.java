1190fd7d4021f3b9b259386732d19d9fd5264087
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-05-10 11:58:37 +0800

    Insert Loader code into opportunity activity

--- app/src/main/java/com/myles/app/aov/OpportunityActivity.java
+++ app/src/main/java/com/myles/app/aov/OpportunityActivity.java
@@ -5,11 +5,27 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
-/**
- * Created by asus on 9/5/2017.
- */
+import android.app.LoaderManager;
+import android.content.ContentUris;
+import android.content.Loader;
+import android.net.Uri;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class OpportunityActivity extends AppCompatActivity {
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class OpportunityActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Opportunity>{
+
+    private static final int OPPS_LOADER = 1;
+
+    private static final String GIS_API_URL_PREFIX = "https://gis-api.aiesec.org/v2/opportunities/";
+
+    private static final String GIS_API_URL_SUFFIX = ".json?access_token=e316ebe109dd84ed16734e5161a2d236d0a7e6daf499941f7c110078e3c75493";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +45,75 @@ public class OpportunityActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.text_country)).setText(country);
         ((TextView)findViewById(R.id.text_duration)).setText(duration);
         ((TextView)findViewById(R.id.text_application_close_date)).setText(applicationCloseDate);
+        
+        setAllDetailViewsText("Loading...");
 
-
-
+        final LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(OPPS_LOADER, null, OpportunityActivity.this);
+        
     }
+    
+    private void setAllDetailViewsText(String str){
+        ((TextView)findViewById(R.id.text_views)).setText(str);
+        ((TextView)findViewById(R.id.text_home_lc)).setText(str);
+        ((TextView)findViewById(R.id.text_managers)).setText(str);
+        ((TextView)findViewById(R.id.text_skills)).setText(str);
+        ((TextView)findViewById(R.id.text_backgrounds)).setText(str);
+        ((TextView)findViewById(R.id.text_visa_link)).setText(str);
+        ((TextView)findViewById(R.id.text_visa_type)).setText(str);
+        ((TextView)findViewById(R.id.text_visa_duration)).setText(str);
+        ((TextView)findViewById(R.id.text_city)).setText(str);
+        ((TextView)findViewById(R.id.text_select_process)).setText(str);
+        ((TextView)findViewById(R.id.text_salary)).setText(str);
+        ((TextView)findViewById(R.id.text_salary_ccy)).setText(str);
+        ((TextView)findViewById(R.id.text_salary_ccy_code)).setText(str);
+        ((TextView)findViewById(R.id.text_create_time)).setText(str);
+        ((TextView)findViewById(R.id.text_update_time)).setText(str);
+    }
+
+    @Override
+    public Loader<Opportunity> onCreateLoader(int id, Bundle args) {
+        Log.v("MylesDebug", "Opportunity: onCreateLoader");
+        URL searchUrl = null;
+        try {
+            searchUrl = new URL(GIS_API_URL_PREFIX + "845422" + GIS_API_URL_SUFFIX);
+        } catch (MalformedURLException e) {
+            setAllDetailViewsText("URL Not Parsed");
+        }
+
+        // 为给定 URL 创建新 loader
+        return new OpportunityDetailAsyncTaskLoader(this, searchUrl);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Opportunity> loader, Opportunity opportunity) {
+        Log.v("MylesDebug", "Opportunity: onLoadFinished");
+        if (opportunity == null) {
+            setAllDetailViewsText("No result.");
+        }
+        
+        ((TextView)findViewById(R.id.text_views)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_home_lc)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_managers)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_skills)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_backgrounds)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_visa_link)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_visa_type)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_visa_duration)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_city)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_select_process)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_salary)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_salary_ccy)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_salary_ccy_code)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_create_time)).setText(opportunity.getHomeLC());
+        ((TextView)findViewById(R.id.text_update_time)).setText(opportunity.getHomeLC());
+        
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Opportunity> loader) {
+        Log.v("MylesDebug", "Opportunity: onResetLoader");
+        // empty the list in activity
+    }    
+    
 }
